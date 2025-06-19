6d716dcd7534ab97759754e5f68c2e91362bce0d
Kravataf <159631603+Kravataf@users.noreply.github.com>
2025-06-19 20:54:18 +0200

    dice rolling

--- main.js
+++ main.js
@@ -25,3 +25,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+function toastPopup() {
+  // Get the toast DIV
+  var x = document.getElementById("toast");
+  var amount = document.getElementById("DICE");
+  var sides = document.getElementById("SIDES");
+  let random = (Math.floor(Math.random() * (sides.value - 1 + 1)) + 1) * amount.value;
+  x.innerText = "You rolled.. " + amount.value + "d" + random;
+
+  x.className = "show";
+
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
