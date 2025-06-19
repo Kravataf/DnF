0ffd45a6b38569a574771627df0f5c30dd48789a
Kravataf <159631603+Kravataf@users.noreply.github.com>
2025-06-19 14:32:35 +0200

    savovanie by teraz malo fungovat

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-// nechcelo to fungovat
-// vzdavam to na teraz :<
+document.addEventListener("DOMContentLoaded", function () {
+  // restore
+  document.querySelectorAll('input, textarea').forEach(el => {
+    // unique keys for each
+    const key = "DnF_" + el.id;
+    if (el.type === "checkbox") {
+      el.checked = localStorage.getItem(key) === "true";
+    } else {
+      const saved = localStorage.getItem(key);
+      if (saved !== null) el.value = saved;
+    }
+    // save
+    el.addEventListener("input", function () {
+      if (el.type === "checkbox") {
+        localStorage.setItem(key, el.checked);
+      } else {
+        localStorage.setItem(key, el.value);
+      }
+    });
+    // save
+    if (el.type === "checkbox") {
+      el.addEventListener("change", function () {
+        localStorage.setItem(key, el.checked);
+      });
+    }
+  });
+});
